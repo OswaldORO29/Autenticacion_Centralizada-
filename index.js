2f974ae53354e06f744fa7d6d2f5ebcf8a5de468
Oswaldo <st4786@utr.edu.mx>
2026-07-02 11:54:32 -0600

    config

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5100;
+const PORT = process.env.PORT || 3000;
+const connectDB = require('./src/config/database');
 
+
+connectDB();
 
 app.listen(PORT, () => {
 	console.log('Hello World');
